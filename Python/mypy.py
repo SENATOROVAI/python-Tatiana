330cd8d7d05843e8cd581b40cdfca24636aebcb5
Mint030 <pas2000me@gmail.com>
2023-10-18 16:53:06 +0300

    Tanya

---
+++ Python/mypy.py
@@ -0,0 +1,20 @@
+# Type Hinting = Статическая типизация =Аннотация типов in python 3.8
+# Раннее авыявление ошибок, отладка кода
+
+x: int = 6
+
+def getValue(value: int) -> int:
+    return value + 5
+
+#print(getValue('5'))
+print(getValue(x))
+
+num: int = 1
+
+def get_Value(value: int) -> bool:
+    return value == 5
+
+#print(getValue('5'))
+print(getValue(x))
+
+get_Value('5')
