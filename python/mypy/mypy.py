2e9d40e0c8e384a5c88dbeb35cf326f4b51fee64
kypena <ptl2022me@gmail.com>
2024-04-21 15:28:36 +0300

    files_renaming

---
+++ python/mypy/mypy.py
@@ -0,0 +1,24 @@
+# Type Hinting = Статическая типизация =Аннотация типов in python 3.8
+# Раннее авыявление ошибок, отладка кода
+
+x: int = 6
+
+
+def get_value(value: int) -> int:
+    """Function printing python version."""
+    return value + 5
+
+
+print(get_value(x))
+
+num: int = 1
+
+
+def get_value2(value: int) -> bool:
+    """Function printing python version."""
+    return value == 5
+
+
+print(get_value2(x))
+
+get_value2(5)
